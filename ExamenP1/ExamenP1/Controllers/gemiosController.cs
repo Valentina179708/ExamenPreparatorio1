8789c0b488fd11916cd8714bd4ecb4c1b1e1d261
Valentina <Valentina@DESKTOP-VV1TMQV>
2020-09-18 14:28:00 -0400

    Con controlador gemios

---
+++ ExamenP1/ExamenP1/Controllers/gemiosController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ExamenP1.Models;
+
+namespace ExamenP1.Controllers
+{
+    public class gemiosController : ApiController
+    {
+        private DataContext db = new DataContext();
+
+        // GET: api/gemios
+        public IQueryable<gemio> Getgemios()
+        {
+            return db.gemios;
+        }
+
+        // GET: api/gemios/5
+        [ResponseType(typeof(gemio))]
+        public IHttpActionResult Getgemio(int id)
+        {
+            gemio gemio = db.gemios.Find(id);
+            if (gemio == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(gemio);
+        }
+
+        // PUT: api/gemios/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Putgemio(int id, gemio gemio)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != gemio.gemioID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(gemio).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!gemioExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/gemios
+        [ResponseType(typeof(gemio))]
+        public IHttpActionResult Postgemio(gemio gemio)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.gemios.Add(gemio);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = gemio.gemioID }, gemio);
+        }
+
+        // DELETE: api/gemios/5
+        [ResponseType(typeof(gemio))]
+        public IHttpActionResult Deletegemio(int id)
+        {
+            gemio gemio = db.gemios.Find(id);
+            if (gemio == null)
+            {
+                return NotFound();
+            }
+
+            db.gemios.Remove(gemio);
+            db.SaveChanges();
+
+            return Ok(gemio);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool gemioExists(int id)
+        {
+            return db.gemios.Count(e => e.gemioID == id) > 0;
+        }
+    }
+}
---
+++ ExamenP1/ExamenP1/Models/DataContext.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace ExamenP1.Models
+{
+    using System.Data.Entity;
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<ExamenP1.Models.gemio> gemios { get; set; }
+    }
+}
